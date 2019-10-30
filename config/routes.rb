f09f311206e1aeefd02ff6fefb81d06623fe1e95
Paulo Rodrigues <paulor2222@gmail.com>
2019-10-30 11:00:47 -0200

    route for profile page add --format doc to rspec

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :accounts
 
   get '/dashboard', to: 'accounts#index'
+  get '/profile', to: 'accounts#show', as: :profile
 
   resources :posts, only: [:new, :create, :show, :destroy]
 
