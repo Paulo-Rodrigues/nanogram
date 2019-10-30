4be8f8ea40e0338f8a1f89712003406789f15135
Paulo Rodrigues <paulor2222@gmail.com>
2019-10-30 12:48:56 -0200

    add some specs for signed in redirects

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,4 +5,8 @@ class RegistrationsController < Devise::RegistrationsController
   def after_sign_up_path_for(account)
     profile_path(account.username)
   end
+
+  # def after_sign_in_path_for(account)
+  #   dashboard_path
+  # end
 end
--- spec/features/users/user_dashboard_spec.rb
+++ spec/features/users/user_dashboard_spec.rb
@@ -1,7 +1,26 @@
 require 'rails_helper'
 
-RSpec.feature 'User Dashboard' do
-  scenario 'logged user go to dashboard' do
+RSpec.feature 'User' do
+  scenario 'Signed up goes to profile' do
+    visit root_path
+    click_link "Sign up"
 
+    fill_in "Username", with: 'Username'
+    fill_in "Email", with: 'test@test.com'
+    fill_in "Password", with: 'password'
+    fill_in "Password confirmation", with: 'password'
+
+    click_button "Sign up"
+
+    expect(page).to have_content('Username')
+  end
+
+  scenario 'Logged user goes to dashboard' do
+    user = create(:account)
+    login_as(user)
+
+    visit root_path
+
+    expect(page).to have_content('show')
   end
 end
