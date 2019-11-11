3ef3eb594bd0fbbf3f7b3dc340c18cf68318ab84
Paulo Rodrigues <paulor2222@gmail.com>
2019-11-11 19:47:57 -0200

    fix feed

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -3,7 +3,7 @@ class AccountsController < ApplicationController
   before_action :set_account, only: [:show]
 
   def index
-    @posts = Post.with_eager_loaded_image.order(created_at: :desc)
+    @posts = current_account.posts.with_eager_loaded_image.order(created_at: :desc)
 
     @follow_suggestions = Account.where.not(id: current_account.followed_ids).limit(8)
   end
